c4883b71fb38869eb538c7c1d2df39c45bec9d9a
JLossavaro <54331912+JLossavaro@users.noreply.github.com>
2022-01-18 18:09:50 -0300

    Update BinaryTreeWords.java

--- src/BinaryTreeWords.java
+++ src/BinaryTreeWords.java
@@ -1,7 +1,6 @@
 /** *************************************************
  * 2 *                                                  *
  * 3 * Júlio Cézar Lossavaro                            *
- * 4 * 2018.0743.029.4                                  *
  * 5 * Implementacão 3                                  *
  * 6 * Disciplina: Estruturas de Dados e Programação I  *
  * 7 * Professor: Ronaldo Fiorilo                       *
